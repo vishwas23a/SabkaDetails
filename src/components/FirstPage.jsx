e7f724c4271e5b723824196443fcf47c4b70bbbf
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-07-01 20:45:14 +0530

    some changes

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -8,8 +8,8 @@ function FirstPage() {
       <div className=' flex w-full h-full bg-slate-200 mt-10 '>
 
         <div className='flex-col  flex  justify-center text-center gap-20'>
-          <h1 className=' text-6xl font-bold text-purple-800 underline underline-offset-2 font-mono'>Sabka Details</h1>
-          <p className='text-2xl'>Here you can see the student details who have registered</p>
+          <h1 className=' text-6xl font-bold underline underline-offset-2 font-mono text-[#86C232]'>Sabka Details</h1>
+          <p className='text-2xl '>Here you can see the student details who have registered</p>
           <div className='w-full flex justify-center '> 
          <NavLink to="Form"> <button
   className="relative py-2 px-10 text-purple-700 text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md shadow-purple-800 hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-purple-500 before:to-purple-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0"
@@ -19,7 +19,7 @@ function FirstPage() {
 </NavLink>
           </div>
         </div>
-        <div className=' h-2/3 w-2/3'>
+        <div className=' h-2/3 w-2/3 '>
           <img src={image1} alt="image" />
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@ import { NavLink, Outlet } from "react-router-dom";
 function Navbar() {
   return (
     <>
-    <div className=" h-14 bg-gray-200 w-full flex justify-between  items-center text-gray-700 ">
+    <div className=" h-14 bg-[#222629] w-full flex justify-between  items-center text-[#86C232] ">
       <div>
-        <h1 className="ml-10">SabkaDetails</h1>
+        <h1 className="ml-10 text-[#86C232]">SabkaDetails</h1>
       </div>
       <div>
         <ul className="flex gap-20 mr-10 ">
-         <NavLink to="/" > <li className="transition ease-in-out delay-50 hover:-translate-y-1 hover:text-black hover:scale-110">Home</li></NavLink>
-         <NavLink to="Form"> <li className="transition ease-in-out delay-50 hover:-translate-y-1 text-black hover:scale-110">Form</li></NavLink>
-        <NavLink to="Details"><li className="transition ease-in-out delay-50 hover:-translate-y-1 text-black hover:scale-110">Card</li></NavLink>  
-        <NavLink to="AllData"> <li className="transition ease-in-out delay-50 hover:-translate-y-1 text-black hover:scale-110">All Students</li></NavLink> 
+         <NavLink to="/" > <li className="transition ease-in-out delay-50 hover:-translate-y-1 text-[#86C232] hover:scale-110">Home</li></NavLink>
+         <NavLink to="Form"> <li className="transition ease-in-out delay-50 hover:-translate-y-1 text-[#86C232] hover:scale-110">Form</li></NavLink>
+        <NavLink to="Details"><li className="transition ease-in-out delay-50 hover:-translate-y-1 text-[#86C232] hover:scale-110">Card</li></NavLink>  
+        <NavLink to="AllData"> <li className="transition ease-in-out delay-50 hover:-translate-y-1 text-[#86C232] hover:scale-110">All Students</li></NavLink> 
         </ul>
       </div>
     </div>
