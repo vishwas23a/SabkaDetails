ade61a3ae967298fdd685dbb0336f77ceb3d5997
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-12-28 01:13:32 +0530

    new

--- src/components/StudentCard.jsx
+++ src/components/StudentCard.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
+import React from "react";
 
 function StudentCard(props) {
-    const {studentName,studentRoll,studentEmail,studentDOB,studentContact,studentCourse}=props;
+  const {
+    studentName,
+    studentRoll,
+    studentEmail,
+    studentDOB,
+    studentContact,
+    studentCourse,
+  } = props;
   return (
     <div>
-      <div className='bg-white shadow-lg  shadow-green-300  drop-shadow-xl  rounded-xl py-8 md:px-6  px-2 '> 
-     
-                <h1 className=' text-green-900 text-center font-bold text-xl'>{studentName}</h1>
-                <ul className='flex flex-col gap-2 mt-4'>
-                    <li className=' text-zinc-800'> <span className='font-bold '>Roll No. :</span>  {studentRoll}</li>
-                    <li className=' text-zinc-800'><span className='font-bold '> Email:</span> {studentEmail}</li>
-                    <li className=' text-zinc-800'> <span className='font-bold '> DOB:</span> {studentDOB}</li>
-                    <li className=' text-zinc-800'> <span className='font-bold '> Contact No. : </span>{studentContact}</li>
-                    <li className=' text-zinc-800'> <span className='font-bold '>Course:</span>  {studentCourse}</li>
-                </ul>
+      <div className="bg-white shadow-lg  shadow-green-300  drop-shadow-xl  rounded-xl py-8 md:px-6 w-96  px-2 ">
+        <h1 className=" text-green-900 text-center font-bold text-xl">
+          {studentName}
+        </h1>
+        <ul className="flex flex-col gap-2 mt-4">
+          <li className=" text-zinc-800">
+            {" "}
+            <span className="font-bold ">Roll No.:</span> {studentRoll}
+          </li>
+          <li className=" text-zinc-800">
+            <span className="font-bold "> Email:</span> {studentEmail}
+          </li>
+          <li className=" text-zinc-800">
+            {" "}
+            <span className="font-bold "> DOB:</span> {studentDOB}
+          </li>
+          <li className=" text-zinc-800">
+            {" "}
+            <span className="font-bold "> Contact No.: </span>
+            {studentContact}
+          </li>
+          <li className=" text-zinc-800">
+            {" "}
+            <span className="font-bold ">Course:</span> {studentCourse}
+          </li>
+        </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default StudentCard
+export default StudentCard;
