5bc35fa0ddf10eeeb7dcb0f9b9aedc7a208b6c48
Vishwas Kumar Verma <vishwasverma23@gmail.com>
2024-12-28 01:06:49 +0530

    improve

--- src/components/AllData.jsx
+++ src/components/AllData.jsx
@@ -1,58 +1,46 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { NavLink } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+import StudentCard from "./StudentCard";
 function AllData() {
-    const [studentData,setStudentData]=useState([]) 
-useEffect(()=>{
-    axios.get('https://sabkadetails-backend.onrender.com/api/users')
-    .then((resp)=>setStudentData(resp.data))
-})
+  const [studentData, setStudentData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://sabkadetails-backend.onrender.com/api/users")
+      .then((resp) => setStudentData(resp.data))
+        .catch((err) => console.log(err));  
+  });
+
 
   return (
     <>
-    <div className=''>
-
-   <div className='flex justify-center'><h1 className=' font-extrabold text-4xl text-green-800 mt-5 underline underline-offset-4 decoration-double	'>All Students Data </h1></div> 
+      <div className="">
+        <div className="flex justify-center">
+          <h1 className=" font-extrabold text-4xl text-green-800 mt-5 underline underline-offset-4 decoration-double	">
+            All Students Data{" "}
+          </h1>
+        </div>
+      </div>
+<div className="flex flex-wrap justify-center gap-20 mt-16 px-24 h-max w-full ">
+    {studentData.map((student) => (
+        <div key={student._id}>
+        <StudentCard 
+        studentName={student.name}
+        studentRoll={student.rollNo}
+        studentEmail={student.email}
+        studentDOB={student.dob}
+        studentContact={student.phoneNo}
+        studentCourse={student.course}
+        /></div>
+    ))
+    
+    }
 
 </div>
-
-
-    <div className='flex flex-col      text-white h-max '>
-
-     
-        <div className='flex justify-center'>
-            <table className='w-11/12   mt-10 '>
-            <thead className='text-purple text-lg font-bold text-white bg-green-800 ' >
-                <tr  >
-                    <th className='p-3  '>Roll No.</th>
-                    <th className=''>Name</th>
-                    <th className=''>Email</th>
-                    <th className=''>DOB</th>
-                    <th className=''>Phone NO.</th>
-                    <th className=''>Course</th>
-            
-                </tr>
-            </thead>
-            <tbody>
-                {
-                studentData.map((value)=>(
-                 <tr className=' bg-gray-800 text-center  text-white border border-green-800' key={value.rollNo}>
-                       <td className='p-3'>{value.rollNo}</td> 
-                       <td >{value.name}</td> 
-                       <td>{value.email}</td> 
-                       <td>{value.dob}</td> 
-                       <td>{value.phoneNo}</td> 
-                       <td>{value.course}</td> 
-                 </tr>
-                ))
-                }
-            </tbody>
-            </table>
-        </div>
       
-    </div>
+      
     </>
-  )
+  );
 }
 
-export default AllData
+export default AllData;
